dd23af1d610652f905b86014f1d48f5307e62487
guspatino <Gus.Patino@CDILLC.com>
2024-02-24 10:19:03 -0500

    Added test comment

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import HelpArea from './HelpArea';
 
+// This is a comment
+
 function MainContent() {
   const [helpVisible, setHelpVisible] = useState(false);
 
